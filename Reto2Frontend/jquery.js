7f502e0909ea9f1ffdcc5862230dd4a41be691fa
SystemFred <fred_Romero001@outlook.com>
2017-02-08 20:26:49 +0900

    Reto 2

---
+++ Reto2Frontend/jquery.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    var divh1=$("div.h1");var divh2=$("div.h2");var divh3=$("div.h3");
+    var diva1=$("div.a1");var diva2=$("div.a2"); var diva3=$("div.a3");var diva4=$("div.a4");
+    var divc1=$("div.c1");var divc2=$("div.c2");var divc3=$("div.c3");
+    var divk1=$("div.k1");var divk2=$("div.k2");var divk3=$("div.k3"); 
+    var divs1=$("div.s1");var divs2=$("div.s2");var divs3=$("div.s3");var divs4=$("div.s4");var divs5=$("div.s5");
+    var divp1=$("div.p1");var divp2=$("div.p2");var divp3=$("div.p3");var divp4=$("div.p4");
+    var diva11=$("div.a11");var diva12=$("div.a12");var diva13=$("div.a13");var diva14=$("div.a14");
+    var divc11=$("div.c11");var divc12=$("div.c12");var divc13=$("div.c13"); 
+    var dive1=$("div.e1");var dive2=$("div.e2");var dive3=$("div.e3");var dive4=$("div.e4");
+
+
+    divh1.animate({height:'200px',width:'40px',marginLeft:'500px'}, 5000);
+    divh2.animate({height:'40px',width:'120px',marginLeft:'500px',marginTop:'80px'}, 5000);
+    divh3.animate({height:'200px',width:'40px',marginLeft:'580px'},5000);
+
+    diva1.animate({height:'200px',width:'40px',marginLeft:'660px'}, 5000);
+    diva2.animate({height:'40px',width:'120px',marginLeft:'660px',marginTop:'80px'}, 5000);
+    diva3.animate({height:'200px',width:'40px',marginLeft:'740px'},5000);
+    diva4.animate({height:'40px',width:'120px',marginLeft:'660px'},5000);
+
+    divc1.animate({height:'200px',width:'40px',marginLeft:'820px'},5000);
+    divc2.animate({height:'40px',width:'120px',marginLeft:'820px'},5000);
+    divc3.animate({height:'40px',width:'120px',marginLeft:'820px',marginTop:'160px'},5000);
+
+    divk1.animate({height:'200px',width:'40px',marginLeft:'980px'},5000);
+    divk2.animate({height:'140px',width:'40px',marginLeft:'1020px'},5000);
+    divk3.animate({height:'130px',width:'40px',marginLeft:'1040px',marginTop:'70px'},5000);
+
+    divs1.animate({height:'40px',width:'120px',marginLeft:'420px',marginTop:'280px'},5000);
+    divs2.animate({height:'120px',width:'40px',marginLeft:'420px',marginTop:'280px'},5000);
+    divs3.animate({height:'40px',width:'120px',marginLeft:'420px',marginTop:'360px'},5000);
+    divs4.animate({height:'120px',width:'40px',marginLeft:'500px',marginTop:'360px'},5000);    
+    divs5.animate({height:'40px',width:'120px',marginLeft:'420px',marginTop:'440px'},5000);
+
+    divp1.animate({height:'200px',width:'40px',marginLeft:'580px',marginTop:'280px'},5000);
+    divp2.animate({height:'40px',width:'120px',marginLeft:'580px',marginTop:'280px',},5000);
+    divp3.animate({height:'40px',width:'120px',marginLeft:'580px',marginTop:'360px'},5000);
+    divp4.animate({height:'120px',width:'40px',marginLeft:'660px',marginTop:'280px'},5000);
+
+    diva11.animate({height:'200px',width:'40px',marginLeft:'740px',marginTop:'280px'},5000);
+    diva12.animate({height:'40px',width:'120px',marginLeft:'740px',marginTop:'280px'},5000);
+    diva13.animate({height:'200px',width:'40px',marginLeft:'820px',marginTop:'280px'},5000);
+    diva14.animate({height:'40px',width:'120px',marginLeft:'740px',marginTop:'360px'},5000);
+
+    divc11.animate({height:'200px',width:'40px',marginLeft:'900px',marginTop:'280px'},5000);
+    divc12.animate({height:'40px',width:'120px',marginLeft:'900px',marginTop:'280px'},5000);
+    divc13.animate({height:'40px',width:'120px',marginLeft:'900px',marginTop:'440px'},5000);
+
+    dive1.animate({height:'200px',width:'40px',marginLeft:'1060px',marginTop:'280px'},5000);
+    dive2.animate({height:'40px',width:'120px',marginLeft:'1060px',marginTop:'280px'},5000);
+    dive3.animate({height:'40px',width:'120px',marginLeft:'1060px',marginTop:'360px'},5000);    
+    dive4.animate({height:'40px',width:'120px',marginLeft:'1060px',marginTop:'440px'},5000);
+
+  });
+});
+  
